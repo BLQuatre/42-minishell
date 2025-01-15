3fb98c190452faddec43d199253762e656c522df
cauvray <cauvray@student.42lehavre.fr>
2025-01-15 07:32:38 +0100

    ~ | Start sub command

--- srcs/core/minishell.c
+++ srcs/core/minishell.c
@@ -6,85 +6,95 @@
 /*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 16:56:12 by cauvray           #+#    #+#             */
-/*   Updated: 2025/01/15 06:50:44 by cauvray          ###   ########.fr       */
+/*   Updated: 2025/01/15 07:31:27 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 #include "holylib.h"
 
-char	*get_str_between(char *msg, char start_chr, char end_chr)
+// char	*get_str_between(char *msg, char start_chr, char end_chr)
+// {
+// 	int in_quotes = false;
+
+// 	int i;
+// 	int j;
+
+// 	i = 0;
+// 	while ((msg[i] && msg[i] != start_chr) || in_quotes)
+// 	{
+// 		if (isquote(msg[i]))
+// 			in_quotes = !in_quotes;
+// 		i++;
+// 	}
+
+// 	j = 0;
+// 	while ((msg[i + j] && msg[i + j] != end_chr) || in_quotes)
+// 	{
+// 		if (isquote(msg[i + j]))
+// 			in_quotes = !in_quotes;
+// 		j++;
+// 	}
+
+// 	// printf("start: `%c` - end: `%c`\n", msg[start], msg[end]);
+// 	if (msg[i] == start_chr && msg[i + j] == end_chr)
+// 		return ft_substr(msg, i + 1, i + j - 1);
+// 	return (ft_strdup("sale merde"));
+// }
+
+
+
+
+
+
+char *get_sub_command(char *input)
 {
-	int in_quotes = false;
+	int i = 0;
+	int in_quotes[2];
+	int parentheses_index = 0;
+	int parenthese_start = 0;
 
-	int i;
-	int j;
-
-	i = 0;
-	while ((msg[i] && msg[i] != start_chr) || in_quotes)
+	ft_bzero(in_quotes, sizeof(bool) * 2);
+	while (input[i])
 	{
-		if (isquote(msg[i]))
-			in_quotes = !in_quotes;
+		if (input[i] == '\'' && !in_quotes[D_QUOTE])
+			in_quotes[S_QUOTE] = !in_quotes[S_QUOTE];
+		if (input[i] == '"' && !in_quotes[S_QUOTE])
+			in_quotes[D_QUOTE] = !in_quotes[D_QUOTE];
+		if (input[i] == '(' && !in_quotes[S_QUOTE] && !in_quotes[D_QUOTE])
+		{
+			if (parenthese_start == 0)
+				parenthese_start = i;
+			parentheses_index++;
+		}
+		if (input[i] == ')' && !in_quotes[S_QUOTE] && !in_quotes[D_QUOTE])
+		{
+			parentheses_index--;
+			if (parentheses_index == 0)
+				break;
+		}
+		printf("dawdaw\n");
 		i++;
 	}
 
-	j = 0;
-	while ((msg[i + j] && msg[i + j] != end_chr) || in_quotes)
-	{
-		if (isquote(msg[i + j]))
-			in_quotes = !in_quotes;
-		j++;
-	}
-
-	// printf("start: `%c` - end: `%c`\n", msg[start], msg[end]);
-	if (msg[i] == start_chr && msg[i + j] == end_chr)
-		return ft_substr(msg, i + 1, i + j - 1);
-	return (ft_strdup("sale merde"));
+	printf("%d %d\n", parenthese_start, i);
+	return (NULL);
 }
 
 
 
-// void	handle_input(char *msg, char start_chr, char end_chr)
-// {
-// 	int quotes;
 
 
-// 	quotes = 0;
 
-// 	int i;
-
-// 	int j;
-
-
-// 	char *test = NULL;
-
-// 	i = 0;
-// 	while (msg[i])
-// 	{
-// 		j = 1;
-// 		if (msg[i] == start_chr && !quotes)
-// 		{
-// 			quotes = !quotes;
-// 			while (msg[i + j] && msg[i + j] != end_chr)
-// 				j++;
-// 			if (msg[i + j] == end_chr)
-// 				test = ft_substr(msg, i, i + j + 1);
-// 			else
-// 				printf("PROBLEM QUOTES\n");
-// 		}
-// 		i++;
-// 	}
-// 	if (test)
-// 		printf("Test quotes value: `%s`\n", test);
-
-// 	free(test);
-// }
 
 void	handle_input(char *msg)
 {
 	if (!is_valid_input(msg))
 		return;
 	printf("[DEBUG] [GOOD] readline: `%s`\n", msg);
+	char *sub = get_sub_command(msg);
+	if (sub)
+		handle_input(sub);
 }
 
 int	main(void)
