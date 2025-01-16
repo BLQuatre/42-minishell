054bc4ad5ec3336970423a29b443716e22d8aafb
NotArt <noterisarthur42@gmail.com>
2025-01-17 00:45:31 +0100

    ~ | Fix cmd_lstadd_back

--- srcs/holylib/cmd_lstadd_back.c
+++ srcs/holylib/cmd_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: anoteris <noterisarthur42@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 00:49:59 by anoteris          #+#    #+#             */
-/*   Updated: 2025/01/15 00:53:29 by anoteris         ###   ########.fr       */
+/*   Updated: 2025/01/17 00:44:09 by anoteris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,16 @@
 
 void	cmd_lstadd_back(t_cmd **lst, t_cmd *new)
 {
+	t_cmd	*last ;
+
 	if (!lst)
 		return ;
 	if (!*lst)
 		*lst = new ;
 	else
-		cmd_lstlast(*lst)->next_cmd = new ;
+	{
+		last = cmd_lstlast(*lst);
+		last->next_cmd = new ;
+		new->prev_cmd = last ;
+	}
 }
