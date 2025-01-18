23b1050c7a1e60b55c7321fb46010d808a2635b6
NotArt <noterisarthur42@gmail.com>
2025-01-18 15:58:54 +0100

    ~ | Use of new free_and_exit holylib fct in my_exit

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: anoteris <noterisarthur42@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 05:55:54 by anoteris          #+#    #+#             */
-/*   Updated: 2025/01/17 07:08:05 by anoteris         ###   ########.fr       */
+/*   Updated: 2025/01/18 15:57:43 by anoteris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,12 @@ void	my_exit(t_cmd *cmd, t_minishell *mini)
 	int	exit_code ;
 
 	if (!cmd->cmd_args[1])
-	{
 		exit_code = mini->exit_code ;
-		minishell_free(mini);
-		cmd_free_lst_from_start(cmd);
-		exit(exit_code);
+	else
+	{
+		exit_code = ft_atoi(cmd->cmd_args[1]);
+		if (exit_code < 0 || exit_code > 255)
+			exit_code = 1 ;
 	}
-	exit_code = ft_atoi(cmd->cmd_args[1]);
-	if (exit_code < 0 || exit_code > 255)
-		exit_code = 1 ;
-	minishell_free(mini);
-	cmd_free_lst_from_start(cmd);
-	exit(exit_code);
+	free_and_exit(cmd, mini, exit_code);
 }
