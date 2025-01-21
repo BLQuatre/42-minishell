010d0ba9ac837fdd65b8f4b34bb242bb9b7b1555
NotArt <noterisarthur42@gmail.com>
2025-01-21 01:10:09 +0100

    ~ | Changed the way cd modify env var to be more POSIX

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: anoteris <noterisarthur42@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 20:33:48 by anoteris          #+#    #+#             */
-/*   Updated: 2025/01/18 06:59:25 by anoteris         ###   ########.fr       */
+/*   Updated: 2025/01/21 01:09:19 by anoteris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,10 @@ static char	*get_target(t_cmd *cmd, t_minishell *mini, int argc)
 	return (target);
 }
 
-static void	update_oldpwd_env(t_minishell *mini)
+static void	update_oldpwd_env(t_minishell *mini, char oldpwd_buf[BUFFER_SIZE])
 {
 	t_env	*old_pwd ;
-	t_env	*pwd ;
 
-	pwd = env_lstget_by_key(mini->env, "PWD");
-	if (!pwd)
-		return ;
 	old_pwd = env_lstget_by_key(mini->env, "OLDPWD");
 	if (!old_pwd)
 	{
@@ -50,7 +46,7 @@ static void	update_oldpwd_env(t_minishell *mini)
 		env_lstadd_back(&mini->env, old_pwd);
 	}
 	free(old_pwd->val);
-	old_pwd->val = ft_strdup(pwd->val);
+	old_pwd->val = ft_strdup(oldpwd_buf);
 }
 
 static void	update_pwd_env(t_minishell *mini)
@@ -76,6 +72,7 @@ void	cd(t_cmd *cmd, t_minishell *mini)
 {
 	char	*target ;
 	int		argc ;
+	char	oldpwd_buf[BUFFER_SIZE];
 
 	argc = get_argc(cmd->cmd_args);
 	if (argc > 2)
@@ -87,12 +84,60 @@ void	cd(t_cmd *cmd, t_minishell *mini)
 	target = get_target(cmd, mini, argc);
 	if (!target)
 		return ;
+	ft_bzero(oldpwd_buf, BUFFER_SIZE) ;
+	if (!getcwd(oldpwd_buf, BUFFER_SIZE))
+		oldpwd_buf[0] = '\0' ;
 	if (chdir(target) != 0)
 	{
 		(write(STDERR_FILENO, "cd: ", 4), perror(target));
 		cmd->exit_code = 1 ;
 		return ;
 	}
-	update_oldpwd_env(mini);
+	update_oldpwd_env(mini, oldpwd_buf);
 	update_pwd_env(mini);
 }
+
+
+// #include "builtins.h"
+// #include <readline/readline.h>
+
+// #define COLOR "\e[0;32m"
+// #define END "\e[0m"
+
+// int main()
+// {
+// 	t_minishell	*mini = minishell_new() ;
+// 	t_cmd		*cmd = cmd_lstnew() ;
+// 	char *line;
+// 	while (true)
+// 	{
+// 		t_env	*PWD = env_lstget_by_key(mini->env, "PWD");
+// 		if (PWD)
+// 			printf("PWD = %s \n", PWD->val);
+// 		t_env	*OLDPWD = env_lstget_by_key(mini->env, "OLDPWD");
+// 		if (OLDPWD)
+// 			printf("OLDPWD = %s \n", OLDPWD->val);
+
+// 		line = readline(COLOR"WildShell $ "END);
+// 		if (!line || !*line)
+// 			continue ;
+// 		cmd->cmd_args = ft_split(line, ' ');
+// 		if (ft_strcmp(cmd->cmd_args[0], "pwd") == 0)
+// 			pwd(cmd);
+// 		else if (ft_strcmp(cmd->cmd_args[0], "cd") == 0)
+// 			cd(cmd, mini);
+// 		else if (ft_strcmp(cmd->cmd_args[0], "env") == 0)
+// 			env(mini);
+// 		else if (ft_strcmp(cmd->cmd_args[0], "echo") == 0)
+// 			echo(cmd);
+// 		else if (ft_strcmp(cmd->cmd_args[0], "exit") == 0)
+// 			my_exit(cmd, mini);
+// 		else if (ft_strcmp(cmd->cmd_args[0], "export") == 0)
+// 			export(cmd, mini);
+// 		else if (ft_strcmp(cmd->cmd_args[0], "unset") == 0)
+// 			unset(cmd, mini);
+// 		free_str_array(cmd->cmd_args);
+// 	}
+// 	minishell_free(mini);
+// 	cmd_free_lst_from_start(cmd);
+// }
