f3c73a9674eb53c118ceb1b1d92144fe15f786ff
NotArt <noterisarthur42@gmail.com>
2025-01-23 08:40:01 +0100

    ~ | Fix: set exit_code to given value in exit_from_child in case of alone builtin

--- srcs/holylib/free_and_exit.c
+++ srcs/holylib/free_and_exit.c
@@ -6,7 +6,7 @@
 /*   By: anoteris <noterisarthur42@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 15:44:55 by anoteris          #+#    #+#             */
-/*   Updated: 2025/01/21 09:51:46 by anoteris         ###   ########.fr       */
+/*   Updated: 2025/01/23 08:38:30 by anoteris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	free_and_exit(t_cmd *cmd, t_minishell *mini, int exit_code)
 void	exit_from_child(t_cmd *cmd, t_minishell *mini, int exit_code)
 {
 	if (IS_ALONE_BUILTIN)
-		cmd->exit_code = 1 ;
+		cmd->exit_code = exit_code ;
 	else
 		free_and_exit(cmd, mini, exit_code);
 }
