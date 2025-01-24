3a8b5aefa918aab1310353208cc8e243d783563b
cauvray <cauvray@student.42lehavre.fr>
2025-01-24 17:57:43 +0100

    + | Check validation on pipe

--- srcs/parsing/check.c
+++ srcs/parsing/check.c
@@ -6,11 +6,13 @@
 /*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 05:17:23 by cauvray           #+#    #+#             */
-/*   Updated: 2025/01/23 19:50:26 by cauvray          ###   ########.fr       */
+/*   Updated: 2025/01/24 17:57:11 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
+
+// TODO: Put error on stderr
 
 static int	is_valid_parentheses(char *input)
 {
@@ -63,7 +65,6 @@ static bool	is_arg_after_redir(char *input)
 	return (true);
 }
 
-// TODO: Put error on stderr
 static bool	is_valid_redir(char *input)
 {
 	int		i;
@@ -82,9 +83,43 @@ static bool	is_valid_redir(char *input)
 		{
 			if (!is_arg_after_redir(input + i))
 			{
-				return (printf(INVALID_TOKEN, ""), false);
+				return (printf(INVALID_TOKEN, ": redir"), false);
 			}
 		}
+	}
+	return (true);
+}
+
+static bool	is_valid_pipe(char *input)
+{
+	int		i;
+	bool	in_quotes[2];
+	bool	pipe_flag;
+
+	i = -1;
+	pipe_flag = false;
+	ft_bzero(in_quotes, sizeof(bool) * 2);
+	while (input[++i])
+	{
+		check_quotes(&in_quotes, input[i]);
+		if (in_quotes[S_QUOTE] || in_quotes[D_QUOTE])
+			continue ;
+		if (input[i] == '|' && input[i + 1] != '|' && !pipe_flag)
+		{
+			if (!input[i + 1])
+				return (printf(INVALID_TOKEN, ": pipe"), false);
+			pipe_flag = true;
+			continue;
+		}
+		if (input[i] == ' ' && pipe_flag)
+		{
+			if (!input[i + 1])
+				return (printf(INVALID_TOKEN, ": pipe"), false);
+			continue;
+		}
+		if (pipe_flag && (input[i] == '&' || input[i] == '|'))
+			return (printf(INVALID_TOKEN, ": pipe"), false);
+		pipe_flag = false;
 	}
 	return (true);
 }
@@ -116,6 +151,8 @@ bool	is_valid_input(char *input)
 	if (!is_valid_quote(input, D_QUOTE))
 		return (printf(INVALID_TOKEN, " '\"'"), false);
 	if (!is_valid_redir(input))
+		return (false);
+	if (!is_valid_pipe(input))
 		return (false);
 	return (true);
 }
