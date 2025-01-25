c47b20f531bc7cf3b143f356dc619c0320e29465
cauvray <cauvray@student.42lehavre.fr>
2025-01-25 03:56:01 +0100

    ~ | Norming in check pipe and andor

--- srcs/parsing/check_andor.c
+++ srcs/parsing/check_andor.c
@@ -6,13 +6,72 @@
 /*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 01:28:08 by cauvray           #+#    #+#             */
-/*   Updated: 2025/01/25 01:38:58 by cauvray          ###   ########.fr       */
+/*   Updated: 2025/01/25 03:50:24 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-bool	is_valid_andor(char	*input)
+// bool	is_valid_andor(char	*input)
+// {
+// 	int		i;
+// 	bool	in_quotes[2];
+// 	bool	andor_flag;
+
+// 	i = -1;
+// 	andor_flag = false;
+// 	ft_bzero(in_quotes, sizeof(bool) * 2);
+// 	while (input[++i])
+// 	{
+// 		check_quotes(&in_quotes, input[i]);
+// 		if (in_quotes[S_QUOTE] || in_quotes[D_QUOTE])
+// 			continue ;
+// 		if ((ft_strncmp(input + i, "&&", 2) == 0
+// 				|| ft_strncmp(input + i, "||", 2) == 0) && !andor_flag)
+// 		{
+// 			if (input[i + 1] && !input[i + 2])
+// 				return (printf(INVALID_TOKEN, ": andor"), false);
+// 			andor_flag = true;
+// 			i++;
+// 			continue ;
+// 		}
+// 		if (input[i] == ' ' && andor_flag)
+// 		{
+// 			if (!input[i + 1])
+// 				return (printf(INVALID_TOKEN, ": andor"), false);
+// 			continue ;
+// 		}
+// 		if (andor_flag && (input[i] == '&' || input[i] == '|'))
+// 			return (printf(INVALID_TOKEN, ": andor"), false);
+// 		andor_flag = false;
+// 	}
+// 	return (true);
+// }
+
+bool	check_andor_conditions(char *input, int *i, bool *andor_flag)
+{
+	if ((ft_strncmp(input + *i, "&&", 2) == 0
+			|| ft_strncmp(input + *i, "||", 2) == 0) && !*andor_flag)
+	{
+		if (input[*i + 1] && !input[*i + 2])
+			return (printf(INVALID_TOKEN, ": andor"), false);
+		*andor_flag = true;
+		(*i)++;
+		return (true);
+	}
+	if (input[*i] == ' ' && *andor_flag)
+	{
+		if (!input[*i + 1])
+			return (printf(INVALID_TOKEN, ": andor"), false);
+		return (true);
+	}
+	if (*andor_flag && (input[*i] == '&' || input[*i] == '|'))
+		return (printf(INVALID_TOKEN, ": andor"), false);
+	*andor_flag = false;
+	return (true);
+}
+
+bool	is_valid_andor(char *input)
 {
 	int		i;
 	bool	in_quotes[2];
@@ -26,24 +85,8 @@ bool	is_valid_andor(char	*input)
 		check_quotes(&in_quotes, input[i]);
 		if (in_quotes[S_QUOTE] || in_quotes[D_QUOTE])
 			continue ;
-		if ((ft_strncmp(input + i, "&&", 2) == 0
-				|| ft_strncmp(input + i, "||", 2) == 0) && !andor_flag)
-		{
-			if (input[i + 1] && !input[i + 2])
-				return (printf(INVALID_TOKEN, ": andor"), false);
-			andor_flag = true;
-			i++;
-			continue ;
-		}
-		if (input[i] == ' ' && andor_flag)
-		{
-			if (!input[i + 1])
-				return (printf(INVALID_TOKEN, ": andor"), false);
-			continue ;
-		}
-		if (andor_flag && (input[i] == '&' || input[i] == '|'))
-			return (printf(INVALID_TOKEN, ": andor"), false);
-		andor_flag = false;
+		if (!check_andor_conditions(input, &i, &andor_flag))
+			return (false);
 	}
 	return (true);
 }
--- srcs/parsing/check_pipe.c
+++ srcs/parsing/check_pipe.c
@@ -6,11 +6,66 @@
 /*   By: cauvray <cauvray@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 01:26:33 by cauvray           #+#    #+#             */
-/*   Updated: 2025/01/25 01:37:33 by cauvray          ###   ########.fr       */
+/*   Updated: 2025/01/25 03:54:46 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
+
+// bool	is_valid_pipe(char *input)
+// {
+// 	int		i;
+// 	bool	in_quotes[2];
+// 	bool	pipe_flag;
+
+// 	i = -1;
+// 	pipe_flag = false;
+// 	ft_bzero(in_quotes, sizeof(bool) * 2);
+// 	while (input[++i])
+// 	{
+// 		check_quotes(&in_quotes, input[i]);
+// 		if (in_quotes[S_QUOTE] || in_quotes[D_QUOTE])
+// 			continue ;
+// 		if (input[i] == '|' && input[i + 1] != '|' && !pipe_flag)
+// 		{
+// 			if (!input[i + 1])
+// 				return (printf(INVALID_TOKEN, ": pipe"), false);
+// 			pipe_flag = true;
+// 			continue ;
+// 		}
+// 		if (input[i] == ' ' && pipe_flag)
+// 		{
+// 			if (!input[i + 1])
+// 				return (printf(INVALID_TOKEN, ": pipe"), false);
+// 			continue ;
+// 		}
+// 		if (pipe_flag && (input[i] == '&' || input[i] == '|'))
+// 			return (printf(INVALID_TOKEN, ": pipe"), false);
+// 		pipe_flag = false;
+// 	}
+// 	return (true);
+// }
+
+bool	check_pipe_conditions(char *input, int *i, bool *pipe_flag)
+{
+	if (input[*i] == '|' && input[*i + 1] != '|' && !*pipe_flag)
+	{
+		if (!input[*i + 1])
+			return (printf(INVALID_TOKEN, ": pipe"), false);
+		*pipe_flag = true;
+		return (true);
+	}
+	if (input[*i] == ' ' && *pipe_flag)
+	{
+		if (!input[*i + 1])
+			return (printf(INVALID_TOKEN, ": pipe"), false);
+		return (true);
+	}
+	if (*pipe_flag && (input[*i] == '&' || input[*i] == '|'))
+		return (printf(INVALID_TOKEN, ": pipe"), false);
+	*pipe_flag = false;
+	return (true);
+}
 
 bool	is_valid_pipe(char *input)
 {
@@ -20,28 +75,14 @@ bool	is_valid_pipe(char *input)
 
 	i = -1;
 	pipe_flag = false;
-	ft_bzero(in_quotes, sizeof(bool) * 2);
+	memset(in_quotes, 0, sizeof(bool) * 2);
 	while (input[++i])
 	{
 		check_quotes(&in_quotes, input[i]);
 		if (in_quotes[S_QUOTE] || in_quotes[D_QUOTE])
 			continue ;
-		if (input[i] == '|' && input[i + 1] != '|' && !pipe_flag)
-		{
-			if (!input[i + 1])
-				return (printf(INVALID_TOKEN, ": pipe"), false);
-			pipe_flag = true;
-			continue ;
-		}
-		if (input[i] == ' ' && pipe_flag)
-		{
-			if (!input[i + 1])
-				return (printf(INVALID_TOKEN, ": pipe"), false);
-			continue ;
-		}
-		if (pipe_flag && (input[i] == '&' || input[i] == '|'))
-			return (printf(INVALID_TOKEN, ": pipe"), false);
-		pipe_flag = false;
+		if (!check_pipe_conditions(input, &i, &pipe_flag))
+			return (false);
 	}
 	return (true);
 }
